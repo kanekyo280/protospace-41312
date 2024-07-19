94d846254d726550c13018f2d0a35d250ebb9d7b
kanekyo280 <kyosukekanemura109@gmail.com>
2024-07-19 12:09:19 +0900

    2点修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,7 +16,7 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path
     else
-      render new:,atatus: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
